e4bb525d97cc3bacff63e0f9dbb509f861751e11
Seal-Trainer <63610573+Seal-Trainer@users.noreply.github.com>
2020-07-03 16:27:29 -0500

    Add files via upload

--- Lower-Bound.cpp
+++ Lower-Bound.cpp
@@ -19,7 +19,15 @@ int main() {
     int number_to_find;
     for (int j = 0; j < number_queries; j++) {
         cin >> number_to_find;
-
+        std::vector<int>::iterator low = lower_bound(v.begin(), v.end(), number_to_find);
+        if (v[low - v.begin()] == number_to_find)
+        {
+            cout << "Yes " << (low - v.begin()+ 1) << endl;
+        }
+        else
+        {
+            cout << "No " << (low - v.begin()+ 1) << endl;   
+        }
     }
     return 0;
 }
