4841151bef2efed8c873b714d3bf979747eb42b0
Seal-Trainer <sealtrainer@protonmail.com>
2020-08-27 18:34:51 -0500

    Finished

--- 10-days-statistics/Quartiles.cpp
+++ 10-days-statistics/Quartiles.cpp
@@ -1,65 +1,23 @@
-#include <cmath>
-#include <cstdio>
-#include <vector>
 #include <iostream>
 #include <algorithm>
-#include <bits/stdc++.h> 
 using namespace std;
-float getingMedian(vector<float> elements, int elementsNumber);
-float getingQ1(vector<float> elements, int elementsNumber);
-float getingQ3(vector<float> elements, int elementsNumber);
 
-
-int main() 
-{ 
-    int numberElements; cin >> numberElements;
-    vector<float> elements;
-    for (int index = 0; index < numberElements; index++)
-    {
-        float element; cin >> element;
-        elements.push_back(element);
+int main() {
+    int elementsNumber;
+    cin >> elementsNumber;
+    int elements[elementsNumber];
+    for(int i = 0; i < elementsNumber; i++)
+        cin >> elements[i];
+    sort(elements, elements + elementsNumber);
+    int q1 = elements[elementsNumber / 4];
+    int q2 = elements[elementsNumber / 2];
+    int q3 = elements[elementsNumber - (elementsNumber / 4 + 1)];
+    if(elementsNumber % 2 == 0)
+        q2 = (q2 + elements[elementsNumber / 2 - 1]) / 2;
+    if(((elementsNumber - (elementsNumber % 2)) / 2) % 2 == 0) {
+        q1 = (q1 + elements[elementsNumber / 4 - 1]) / 2;
+        q3 = (q3 + elements[elementsNumber - (elementsNumber / 4)]) / 2;
     }
-
-    // Sorting the vector
-    sort(elements.begin(), elements.end()); 
-
-    float q1 = getingQ1(elements, elements.size()/2);
-    float q2 = getingMedian(elements, numberElements);
-    float q3 = getingQ3(elements);
-
-    cout << q1 << endl;
-    cout << q2 << endl;
-    cout << q3 << endl;
-    return 0; 
-} 
-
-
-float getingMedian(vector<float> elements, int elementsNumber)
-{
-    
-    float median;
-
-    if(elementsNumber % 2 != 0)
-    {
-        int medianIndex = 0;
-        medianIndex = elementsNumber / 2;
-        median = elements.at(medianIndex - 1);
-        return median;
-    }
-    else
-    {
-        int medianIndex; medianIndex = elementsNumber / 2;
-        median = elements.at(medianIndex - 1) +  elements.at(medianIndex);
-        return median / 2;
-    }   
-}
-
-float getingQ1(vector<float> elements, int elementsNumber)
-{
-    float q1 = getingMedian(elements, elementsNumber);
-    return q1;
-}
-float getingQ3(vector<float> elements)
-{
-    float q3
+    cout << q1 << "\n" << q2 << "\n" << q3 << "\n";
+    return 0;
 }
